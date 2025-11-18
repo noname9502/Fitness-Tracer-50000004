68aa78b04b426798fa2fad9e41ac7dcd3032f035
noname9502 <108085711+noname9502@users.noreply.github.com>
2025-11-18 07:01:06 -0800

    Update app.py
    Refactor Flask app to use environment variables for MySQL configuration

--- app.py
+++ app.py
@@ -14,10 +14,10 @@ load_dotenv()
 app = Flask(__name__)
 
 # MySQL Configuration
-app.config['MYSQL_HOST'] = ''
-app.config['MYSQL_USER'] = ''
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = ''
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
 
 # Secure session management configurations
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY') # Needed for session management
